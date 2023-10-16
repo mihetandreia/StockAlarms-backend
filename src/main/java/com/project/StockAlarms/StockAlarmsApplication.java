5b194d316abffa16598216940df029f4d7f2d975
mihetandreia <andreia.mihet@gmail.com>
2023-10-16 11:19:37 +0300

    User class created

---
+++ src/main/java/com/project/StockAlarms/StockAlarmsApplication.java
@@ -0,0 +1,14 @@
+package com.project.StockAlarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlarmsApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlarmsApplication.class, args);
+	}
+
+}
